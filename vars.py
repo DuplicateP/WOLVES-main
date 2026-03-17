86fad63d41bf5856705f2faae8888bc68f38993c
DuplicateP <modassirjawaid44@gmail.com>
2026-03-17 06:03:15 +0530

    Update API credentials in vars.py
    Updated API_ID, API_HASH, and BOT_TOKEN with new values.

--- vars.py
+++ vars.py
@@ -3,7 +3,7 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "26792945"))
+API_HASH = environ.get("API_HASH", "6be4bc36c6a70c269f80a7388e59e150")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8579870981:AAEriMx2wszmZqEipEv3Zfim6cz0qmH_od8")
 AUTH_USERS = "7357284659"
